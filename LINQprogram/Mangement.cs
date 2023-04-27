71d2bc92c5815d3447dfcf5090c6ec1695643e73
Azaruddin8297 <ajarpopo.com>
2023-04-27 22:48:24 +0530

    Linq

---
+++ LINQprogram/Mangement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQprogram
+{
+    public class Mangement
+    {
+        public void topRecords(List<ProductModel> products)
+        {
+            var RecordData = (from Product in products
+                             orderby Product.Rating descending
+                             select Product).Take(3);
+            foreach (var record in RecordData)
+            {
+                Console.WriteLine(record.ProductId+" "+record.UserId+" "+record.Rating+" "+record.IsLike);
+            }
+        }
+        public void SelectedRecords(List<ProductModel> productsModel)
+        {
+            var RecordData = from Product in productsModel
+                             where (Product.ProductId == 1 && Product.Rating >3)||
+                             (Product.ProductId == 4 && Product.Rating>3 )||
+                             (Product.ProductId == 9 && Product.Rating > 3)
+                             select Product;
+            foreach (var record in RecordData)
+            {
+                Console.WriteLine(record.ProductId + " " + record.UserId + " " + record.Rating + " " + record.IsLike);
+            }
+        }
+        public void RetriewCountofRecords(List<ProductModel> products)
+        {
+            var RecordData = products.GroupBy(x => x.ProductId).Select(x => new { productId = x.Key, count = x.Count() });
+            foreach (var record in RecordData)
+            {
+                Console.WriteLine(record.productId + " " + record.count);
+            }
+        }
+        public void RetriewIdandReview(List<ProductModel> products)
+        {
+            var RecordData = from product in products
+                             select
+                             new { productid = product.ProductId, review = product.Review };
+            foreach (var record in RecordData)
+            {
+                Console.WriteLine(record.productid+ " " + record.review);
+            }
+        }
+    }
+}
---
+++ LINQprogram/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LINQprogram
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            List<ProductModel> products = new List<ProductModel>()
+            { new ProductModel {ProductId = 1,UserId = 10 ,Rating = 4,IsLike = true,Review = "Good"},
+              new ProductModel {ProductId = 2,UserId = 20 ,Rating = 4.5,IsLike = true,Review = "Good"},
+              new ProductModel {ProductId = 3,UserId = 30 ,Rating = 2,IsLike = false,Review = "Bad"},
+              new ProductModel {ProductId = 3,UserId = 40 ,Rating = 5.5,IsLike = true,Review = "Good"},
+              new ProductModel {ProductId = 5,UserId = 50 ,Rating = 2.2,IsLike = false,Review = "Bad"},
+              new ProductModel {ProductId = 5,UserId = 60 ,Rating = 6.6,IsLike = true,Review = "Good"},
+              new ProductModel {ProductId = 7,UserId = 70 ,Rating = 6.7,IsLike = true,Review = "Good"},
+              new ProductModel {ProductId = 7,UserId = 80 ,Rating = 9.2,IsLike = true, Review = "Good"},
+              new ProductModel {ProductId = 7,UserId = 90 ,Rating = 2.3,IsLike = false,Review = "Bad"}
+            };
+            Mangement Mangement = new Mangement();
+            // Mangement.topRecords(products);
+            //Mangement.SelectedRecords(products);
+            // Mangement.RetriewCountofRecords(products);
+            Mangement.RetriewIdandReview(products);
+        }  
+
+    }
+}
